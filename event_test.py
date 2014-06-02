f2e5232702b41b28ce83349df80edfeee97a0c70
Marianna Vladimirova <marianna.vl.vladimirova@gmail.com>
2014-06-02 10:25:55 +0300

    pep8 tested

--- event_test.py
+++ event_test.py
@@ -1,6 +1,7 @@
 import unittest
 import event
 
+
 class EventTest(unittest.TestCase):
     def test_event_instance(self):
         ev = event.Event("18.03.2014", "FMI", "18:00", "izpit")
@@ -9,10 +10,10 @@ class EventTest(unittest.TestCase):
         self.assertEqual(ev.hour, "18:00")
         self.assertEqual(ev.name, "izpit")
 
-    
     def test_to_string(self):
-         ev = event.Event("18.03.2014", "FMI", "18:00", "izpit")
-         self.assertEqual(str(ev), "izpit will be held at FMI, on 18.03.2014 at 18:00")
+        ev = event.Event("18.03.2014", "FMI", "18:00", "izpit")
+        self.assertEqual(str(ev),
+                         "izpit will be held at FMI, on 18.03.2014 at 18:00")
 
 
 if __name__ == '__main__':
