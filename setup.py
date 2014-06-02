9c5c1ddd11f04224e85936cbf9e6700ac1a51e8a
Marianna Vladimirova <marianna.vl.vladimirova@gmail.com>
2014-06-02 10:56:44 +0300

    setup file

---
+++ setup.py
@@ -0,0 +1,5 @@
+setup(name = 'MyOrganizer',
+      version = '1.0',
+      description = 'An organizer application',
+      author = 'Marianna Vladimirova',
+      author_email = 'marianna.vl.vladimirova@gmail.com')
