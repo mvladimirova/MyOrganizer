d3f952ebfe98f546806f4ba4b531f5f07c9e2f3e
Marianna Vladimirova <marianna.vl.vladimirova@gmail.com>
2014-06-25 12:04:00 +0300

    checked with pep8

--- mainstruct/event_test.py
+++ mainstruct/event_test.py
@@ -16,7 +16,7 @@ class EventTest(unittest.TestCase):
         self.assertEqual(str(ev),
                          "izpit will be held at FMI, on 18.3.2014 at 18:00")
 
-       
+
 class DaylyEventTests(unittest.TestCase):
     def test_dayly_instance(self):
         dayly = event.Dayly("23.12.2014", "home", "08:00", "write code", 1)
@@ -30,13 +30,14 @@ class DaylyEventTests(unittest.TestCase):
         dayly = event.Dayly("23.12.2014", "home", "08:00", "write code", 1)
         self.assertEqual(str(dayly),
                          "every 1 day/s starting 23.12.2014")
-    
+
     def test_occurs_on_date(self):
         dayly = event.Dayly("23.12.2014", "home", "08:00", "write code", 1)
         self.assertEqual(dayly.occurs_on_date("24.12.2014"), True)
 
         dayly2 = event.Dayly("23.12.2014", "home", "08:00", "write code", 2)
         self.assertEqual(dayly2.occurs_on_date("24.12.2014"), False)
+
 
 class WeeklyEventTest(unittest.TestCase):
     def test_weekly_instance(self):
@@ -49,14 +50,14 @@ class WeeklyEventTest(unittest.TestCase):
     def test_weekly_str(self):
         w = event.Weekly("FMI", "18:00", "python", 1)
         self.assertEqual(str(w), "every tuesday")
-        w1 = event.Weekly("FMI", "18:00", "python", 0,2)
+        w1 = event.Weekly("FMI", "18:00", "python", 0, 2)
         self.assertEqual(str(w1), "every monday, wednesday")
 
     def test_occurs_on_date(self):
         w = event.Weekly("FMI", "18:00", "python", 1)
         self.assertEqual(w.occurs_on_date("30.6.2014"), False)
         self.assertEqual(w.occurs_on_date("1.7.2014"), True)
-        w1 = event.Weekly("FMI", "18:00", "python", 0,2)
+        w1 = event.Weekly("FMI", "18:00", "python", 0, 2)
         self.assertEqual(w1.occurs_on_date("29.6.2014"), False)
         self.assertEqual(w1.occurs_on_date("25.6.2014"), True)
         self.assertEqual(w1.occurs_on_date("30.6.2014"), True)
@@ -74,14 +75,13 @@ class MonthlyEventTest(unittest.TestCase):
         self.assertEqual(m.day, 18)
         self.assertEqual(m.name, "another month since we are together")
         self.assertEqual(m.month, None)
-    
-    
+
     def test_first_str(self):
         m = event.Monthly(18, "mom's birthday", 11)
         self.assertEqual(str(m), "18-th of each november")
 
     def test_second_str(self):
-        m = event.Monthly(18,"another month since we are together")
+        m = event.Monthly(18, "another month since we are together")
         self.assertEqual(str(m), "18-th of each month")
 
     def test_first_occurs_on_date(self):
